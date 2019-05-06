5b8e2d90975b2338f341148e126447d1c74228a3
fienestar <47744803+fienestar@users.noreply.github.com>
2019-05-06 17:33:57 +0900

    메크로 추가

---
+++ acmicpc.net/$template.cpp
@@ -0,0 +1,16 @@
+// 사용된 알고리즘: 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef LOCAL
+#define LOCAL(x) ((void)0)
+#endif
+
+int main()
+{
+    LOCAL(OUTPUT_TO_FILE);
+    LOCAL(INPUT_FROM_FILE);
+    // Code here!
+}
--- acmicpc.net/2439.cpp
+++ acmicpc.net/2439.cpp
@@ -2,12 +2,18 @@
 
 using namespace std;
 
+#ifndef LOCAL
+#define LOCAL(x) ((void)0)
+#endif
+
 void PutChars(char c,size_t n){
     while(n--)fputc(c,stdout);
 }
 
 int main()
 {
+    LOCAL(OUTPUT_TO_FILE);
+    LOCAL(INPUT_FROM_FILE);
     size_t N;
     cin>>N;
     for(size_t i=0;i!=N;++i){
